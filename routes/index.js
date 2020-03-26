19bd7208ba16ad0408b26d4b4732f732f9aa5496
Andrey Bogdanov <bay73@mail.ru>
2020-03-26 17:17:51 +0100

    Add card for daily puzzle

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 // Welcome Page
-router.get('/', (req, res) => res.render('welcome'));
+router.get('/', (req, res) => 
+  res.render('welcome', {
+    user: req.user,
+    puzzle: {
+      type: {
+        code: "tapa_classic",
+        name: "Tapa",
+        rules: "Shade some empty cells black to create a single connected wall. Numbers in a cell indicate the length of consecutive shaded blocks in the neighboring cells. If there is more than one number in a cell, then there must be at least one white (unshaded) cell between the black cell groups.",
+      }
+    }
+  }));
 
 module.exports = router;
