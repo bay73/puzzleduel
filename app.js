b004b900dfa3fdc650fffafd6534a8842000d54f
Andrey Bogdanov <bay73@mail.ru>
2020-03-26 22:04:10 +0100

    Connect to Mongo DB and User model

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
 
 // EJS
 app.use(expressLayouts);
