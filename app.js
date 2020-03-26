c5f1621d3c67317c3c384361e877ba2de2212a7d
Andrey Bogdanov <bay73@mail.ru>
2020-03-26 17:01:09 +0100

    Routes for static content

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use('/', require('./routes/index.js'));
 
+// Static content
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/js', express.static(__dirname + '/js'));
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
