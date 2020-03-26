691c8f0ca8043df1041518ba9a2f85f9a7c241d6
Andrey Bogdanov <bay73@mail.ru>
2020-03-27 00:50:25 +0100

    Start button processing

--- js/square_puzzle.js
+++ js/square_puzzle.js
@@ -1,8 +1,10 @@
 // Default puzzle with square grid.
-var squarePuzzle = function(typeCode) {
+var squarePuzzle = function(typeCode, id) {
+  this.id = id;
   this.typeCode = typeCode;
   this.rows = 7
   this.cols = 7;
+  this.initImages();
   this.createBoard();
 }
 
@@ -36,12 +38,17 @@ squarePuzzle.prototype.render = function(snap) {
 squarePuzzle.prototype.renderCell = function(cell) {
   var x = this.leftGap + cell.col*this.cellSize;
   var y = this.topGap + cell.row*this.cellSize;
-  var rect = this.snap.rect(x, y, this.cellSize, this.cellSize);
-  rect.attr({
+  cell.rect = this.snap.rect(x, y, this.cellSize, this.cellSize);
+  cell.rect.attr({
     fill: "none",
     stroke: "#000",
     strokeWidth: 1
   });
+  cell.element = this.snap.image(this.imageUrl(cell.value), x, y, this.cellSize, this.cellSize);
+}
+
+squarePuzzle.prototype.syncCell = function(cell) {
+  cell.element.attr({href: this.imageUrl(cell.value)});
 }
 
 squarePuzzle.prototype.createBoard = function() {
@@ -49,13 +56,41 @@ squarePuzzle.prototype.createBoard = function() {
   for (var y = 0; y < this.rows; y++) {
     this.cells[y] = new Array(this.cols);
     for (var x = 0; x < this.cols; x++) {
-      if (x==y) {
-        this.cells[y][x] = {col: x, row: y, value: 0};
-      } else {
-        this.cells[y][x] = {col: x, row: y, value: 0};
-      }
+      this.cells[y][x] = {col: x, row: y, value: "white", togglers: this.togglers};
     }
   }
 }
 
+squarePuzzle.prototype.start = function() {
+  data = {
+    'a1': '1_1',
+    'b2': '3_3',
+    'c3': '1_1_3',
+    'd4': '1_1_3',
+    'e5': '1_1_3',
+  }
+  for (const [key, value] of Object.entries(data)) {
+    var x = key.charCodeAt(0) - 'a'.charCodeAt(0);
+    var y = parseInt(key.substring(1)) - 1;
+    this.cells[y][x].value = value;
+    this.cells[y][x].togglers = [];
+    this.syncCell(this.cells[y][x]);
+  }
+}
 
+squarePuzzle.prototype.initImages = function() {
+  if(this.typeCode == "tapa_classic") {
+    this.clues = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "1_1", "1_2", "1_3", "1_4", "1_5", "2_2", "2_3", "2_4", "3_3", "1_1_1", "1_1_2", "1_1_3", "1_2_2", "1_1_1_1"];
+    this.togglers = ["white", "black", "x"];
+  }
+  this.preloadImages(this.clues);
+  this.preloadImages(this.togglers);
+}
+
+squarePuzzle.prototype.preloadImages = function(imageList) {
+  imageList.forEach(name => new Image().src = this.imageUrl(name));
+}
+
+squarePuzzle.prototype.imageUrl = function(imageName) {
+  return "images/"+imageName+".png";
+}
