711d23fc33e4c9f673af0da8f0d61d0aa6b4b1aa
Andrey Bogdanov <bay73@mail.ru>
2020-03-27 19:37:10 +0100

    Read daily puzzle desc from database

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,31 @@
 const express = require('express');
 const router = express.Router();
+// Puzzle model
+const Puzzle = require('../models/Puzzle');
 // PuzzleType model
 const PuzzleType = require('../models/PuzzleType');
 
 // Welcome Page
 router.get('/', (req, res) => { 
-  PuzzleType.findOne({ code: 'tapa_classic' }).then(type => {
-    if(type) {
-      var puzzle = {
-        type: type
-      }
+  var datetime = new Date();
+  Puzzle.findOne({daily: datetime.toISOString().slice(0,10)}, "-data").then(puzzle => {
+    if (puzzle) {
+      var puzzleObj = puzzle.toObject();
+      PuzzleType.findOne({ code: puzzleObj.type }).then(type => {
+        if(type) {
+          puzzleObj.type = type.toObject();
+        }
+        res.render('welcome', {
+          user: req.user,
+          puzzle: puzzleObj
+        });
+      });
+    } else {
+      res.render('welcome', {
+        user: req.user,
+        puzzle: null
+      });
     }
-    res.render('welcome', {
-      user: req.user,
-      puzzle: puzzle
-    });
   });
 });
 
