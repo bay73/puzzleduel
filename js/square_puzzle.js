8c4a773a284de55501061c3bde23417f497a8380
Andrey Bogdanov <bay73@mail.ru>
2020-03-29 20:15:42 +0200

    Add images for minesweeper

--- js/square_puzzle.js
+++ js/square_puzzle.js
@@ -211,6 +211,10 @@ squarePuzzle.prototype.initImages = function() {
     this.clues = ["black_circle", "white_circle"];
     this.togglers = ["white", "black_circle", "white_circle"];
   }
+  if(this.typeCode == "minesweeper_classic") {
+    this.clues = ["0", "1", "2", "3", "4", "5", "6", "7", "8"];
+    this.togglers = ["white", "mine", "cross"];
+  }
   this.preloadImages(this.clues);
   this.preloadImages(this.togglers);
 }
