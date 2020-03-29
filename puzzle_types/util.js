b87fe9ea6ece02edb3bf761e497e69337ec239a2
Andrey Bogdanov <bay73@mail.ru>
2020-03-29 13:45:13 +0200

    Extract common functions to util library

---
+++ puzzle_types/util.js
@@ -0,0 +1,114 @@
+
+const ConnectedChecker = {
+  findFirst: function(cells, color) {
+    for (var y = 0; y < cells.rows; y++) {
+      for (var x = 0; x < cells.cols; x++) {
+        if (cells[y][x] == color){
+         return {x:x, y:y};
+        }
+      }
+    }
+    return null;
+  },
+
+  fill: function(cells, filled, position, color){
+    if (!filled[position.y][position.x] && cells[position.y][position.x] == color) {
+      filled[position.y][position.x] = true;
+      if (position.y > 0) this.fill(cells, filled, {x: position.x, y:position.y - 1}, color);
+      if (position.x > 0) this.fill(cells, filled, {x: position.x - 1, y:position.y}, color);
+      if (position.y + 1 < cells.rows) this.fill(cells, filled, {x: position.x, y:position.y + 1}, color);
+      if (position.x + 1 < cells.cols) this.fill(cells, filled, {x: position.x + 1, y:position.y}, color);
+    }
+  },
+
+  checkAllFilled: function(cells, filled, color){
+    for (var y = 0; y < cells.rows; y++) {
+      for (var x = 0; x < cells.cols; x++) {
+        if (cells[y][x] == color && !filled[y][x]){
+          return false;
+        }
+      }
+    }
+    return true;
+  }
+};
+
+const Util = {
+  create2DArray: function(rows, cols, defaultValue) {
+    // Create 2D array, used by other method.
+    var cells = [];
+    for (var y = 0; y < rows; y++) {
+      cells[y] = new Array(cols);
+      for (var x = 0; x < cols; x++) {
+        cells[y][x] = defaultValue;
+      }
+    }
+    cells.rows = rows;
+    cells.cols = cols;
+    return cells;
+  },
+
+  parseDimension: function(dimension) {
+    // Parse dimension string to values.
+    var dimensions = dimension.split("x");
+    return {
+      rows: dimensions[1],
+      cols: dimensions[0]
+    } 
+  },
+
+  parseCoord: function(coord) {
+    // Parse coordinate string to tuple.
+    return {
+      x: coord.charCodeAt(0) - 'a'.charCodeAt(0),
+      y: parseInt(coord.substring(1)) - 1
+    }
+  },
+
+  coord: function(x, y) {
+    // Convert coordinate tuple to string.
+    return String.fromCharCode('a'.charCodeAt(0) + x) + (y+1).toString();
+  },
+
+  checkConnected: function(cells, color) {
+    // Returns true if all cells of one color are orthogonally connected.
+    // Returns false if not.
+    var first = ConnectedChecker.findFirst(cells, color);
+    if(first) {
+      var filled = Util.create2DArray(cells.rows, cells.cols, false)
+      ConnectedChecker.fill(cells, filled, first, color);
+      if (!ConnectedChecker.checkAllFilled(cells, filled, color)) {
+        return false;
+      }
+    }
+    return true;
+  },
+
+  findWrongValue: function(cells, goodValues) {
+    // Returns one cell not having good value.
+    // Returns null if all cells have good value.
+    for (var y = 0; y < cells.rows; y++) {
+      for (var x = 0; x < cells.cols; x++) {
+        if (!goodValues.includes(cells[y][x])){
+          return Util.coord(x,y);
+        };
+      }
+    }
+    return null;
+  },
+
+  find2x2: function(cells, color) {
+    // Returns list of cells forming 2x2 square.
+    // Returns null if no 2x2 squares of the given color.
+    for (var y = 0; y < cells.rows - 1; y++) {
+      for (var x = 0; x < cells.cols - 1; x++) {
+        if (cells[y][x] == color && cells[y+1][x] == color && cells[y][x+1] == color && cells[y+1][x+1] == color){
+          return [Util.coord(x,y), Util.coord(x+1,y), Util.coord(x,y+1), Util.coord(x+1,y+1)];
+        }
+      }
+    }
+    return null;
+  }
+};
+
+module.exports = Util;
