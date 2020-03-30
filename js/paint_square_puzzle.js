92708a05d7d6f0b20a46cb212b321f8ad3e67b4e
Andrey Bogdanov <bay73@mail.ru>
2020-03-30 21:23:34 +0200

    Reduce height of the starting page

--- js/paint_square_puzzle.js
+++ js/paint_square_puzzle.js
@@ -47,7 +47,7 @@ innerCluePuzzle.prototype.createBoard = function() {
 innerCluePuzzle.prototype.findCellSize = function() {
   // Find cell size based on size of the window.
   var hSizeLimit = this.snap.node.clientWidth*0.97;
-  var vSizeLimit = window.innerHeight*0.6;
+  var vSizeLimit = window.innerHeight*0.57;
   this.cellSize = Math.min(hSizeLimit / this.cols, vSizeLimit / this.rows);
   this.leftGap = (this.snap.node.clientWidth - this.cellSize * this.cols)/2;
   this.topGap = 1;
