79a11e3173081f3a73a0b0102b4590079deca670
Andrey Bogdanov <bay73@mail.ru>
2020-04-06 00:03:07 +0200

    Fix area checker for sudoku

--- puzzle_types/sudoku_util.js
+++ puzzle_types/sudoku_util.js
@@ -16,27 +16,40 @@ checkColumnMagic: function(cells, colors) {
   return {status: "OK"};
 },
 checkAreaMagic: function(cells, colors) {
-  if (this.rows == 9) {
+  if (cells.rows == 9) {
     for (var j = 0; j < 3; j++) {
       for (var i = 0; i < 3; i++) {
         var res = SudokuUtil.checkOnceInArea(cells, i*3, i*3 + 3, j*3, j*3 + 3, colors);
         if (res){
           return {status: "All digits should be exactly once in every area", errors: res};
         }
-        return {status: "OK"};
       }
     }
-  } else {
+    return {status: "OK"};
+  }
+  if (cells.rows == 8) {
+    for (var j = 0; j < 4; j++) {
+      for (var i = 0; i < 2; i++) {
+        var res = SudokuUtil.checkOnceInArea(cells, i*4, i*4 + 4, j*2, j*2 + 2, colors);
+        if (res){
+          return {status: "All digits should be exactly once in every area", errors: res};
+        }
+      }
+    }
+    return {status: "OK"};
+  }
+  if (cells.rows == 6) {
     for (var j = 0; j < 3; j++) {
       for (var i = 0; i < 2; i++) {
         var res = SudokuUtil.checkOnceInArea(cells, i*3, i*3 + 3, j*2, j*2 + 2, colors);
         if (res){
           return {status: "All digits should be exactly once in every area", errors: res};
         }
-        return {status: "OK"};
       }
     }
+    return {status: "OK"};
   }
+  return {status: "OK"};
 },
 checkOnceInArea: function(cells, xFrom, xTo, yFrom, yTo, colors) {
   var positionsToCheck = [];
