b5b259314f6615265c7dd8ae368056f3d398e3e5
Andrey Bogdanov <bay73@mail.ru>
2020-05-03 22:26:01 +0200

    Compare user log

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,7 @@ const util = require('../utils/puzzle_util');
 const ensureAuthenticated = require('../config/auth').ensureAuthenticated;
 
 // List of daily puzzles
-router.get('/actionlog', ensureAuthenticated, async (req, res, next) => { 
+router.get('/actionlog', ensureAuthenticated, async (req, res, next) => {
   try {
     var users = req.query.users.split(',');
     var userData = await User.find({_id: {$in: users}});
@@ -18,7 +18,44 @@ router.get('/actionlog', ensureAuthenticated, async (req, res, next) => {
       logData: log.map(logItem => logItem.toObject())
     });
   } catch (e) {
-    next(e) 
+    next(e)
+  }
+});
+
+// List of daily puzzles
+router.get('/userips', ensureAuthenticated, async (req, res, next) => {
+  try {
+    var d = new Date();
+    d.setDate(d.getDate() - 10);
+    var userMap = {}
+    var userData = await User.find();
+    userData.forEach(user => userMap[user._id] = user);
+    var log = await UserActionLog.aggregate([{
+      $match : {"ipInfo.ip": {$exists: true}, "date": {$gt: d}}
+    },{
+      $group: {
+        _id: {ip:"$ipInfo.ip", userId:"$userId"},
+        country: { $min: "$ipInfo.country"},
+        region: { $min: "$ipInfo.region"},
+        count: { $sum: 1 }
+      }
+    }]);
+    res.render('userips', {
+      user: req.user,
+      logData: log.map(logItem => {
+        return {
+          country: logItem.country,
+          region: logItem.region,
+          ip: logItem._id.ip,
+          userId: logItem._id.userId,
+          userName: userMap[logItem._id.userId].name,
+          userEmail: userMap[logItem._id.userId].email,
+          count: logItem.count
+        };
+      })
+    });
+  } catch (e) {
+    next(e)
   }
 });
 
