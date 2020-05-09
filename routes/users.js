a3a74a33415c56fc16b669a5305cc4d8397a7253
Andrey Bogdanov <bay73@mail.ru>
2020-05-09 19:14:02 +0200

    Remove mailgun

--- routes/users.js
+++ routes/users.js
@@ -12,10 +12,6 @@ var recaptcha = new Recaptcha(recaptchaKeys.siteKey, recaptchaKeys.secret);
 // Config email transporter
 const transporter = nodemailer.createTransport(require('./../config/keys').email);
 
-var API_KEY = process.env.MAILGUN_API_KEY;
-var DOMAIN = process.env.MAILGUN_DOMAIN;
-var mailgun = require('mailgun-js')({apiKey: API_KEY, domain: DOMAIN});
-
 
 // Load User model
 const User = require('../models/User');
@@ -313,9 +309,7 @@ router.post('/reset', recaptcha.middleware.verify, recaptcha.middleware.render, 
 
         await user.save();
 
-//        await transporter.sendMail(mailOptions);
-
-        await mailgun.messages().send(mailOptions);
+        await transporter.sendMail(mailOptions);
 
       }
 
