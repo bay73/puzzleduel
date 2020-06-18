82d31137fbbfb243ce91432a30f403d2fdba7995
Andrey Bogdanov <bay73@mail.ru>
2020-06-18 14:07:54 +0200

    Fix sorting

--- routes/rating.js
+++ routes/rating.js
@@ -33,7 +33,7 @@ router.get('/:ratingdate', async (req, res, next) => {
           details: details
         };
       }),
-      sortColumn: req.query.sort || Math.random() > 0.33?"rating":"success"
+      sortColumn: req.query.sort || "rating"
     }); 
   } catch (e) {
     next(e);
