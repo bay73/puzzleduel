d9395fe4c66afa383704c7b40c3fa2c7241e96b7
Andrey Bogdanov <bay73@mail.ru>
2020-06-19 02:24:10 +0200

    Recount contest utility

---
+++ utils/contest_run.js
@@ -0,0 +1,29 @@
+const recountContest = require('../utils/contest');
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// DB Config
+const db = require('../config/keys').mongoURI;
+
+recount = async function () {
+  await recountContest('40645r1kb6ih7hk');
+}
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log('MongoDB Connected')
+    recount().then(() => {
+      mongoose.disconnect();
+    });
+  })
+  .catch(err => console.log(err));
+
+
+
