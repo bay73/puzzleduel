96263e58b4c5af836a8cd69baa2a5d500b082068
Andrey Bogdanov <bay73@mail.ru>
2020-09-06 15:03:28 +0200

    Change sorting at the rating page

--- routes/rating.js
+++ routes/rating.js
@@ -33,7 +33,7 @@ router.get('/:ratingdate', async (req, res, next) => {
           details: details
         };
       }),
-      sortColumn: req.query.sort || "rating"
+      sortColumn: req.query.sort || "success"
     }); 
   } catch (e) {
     next(e);
