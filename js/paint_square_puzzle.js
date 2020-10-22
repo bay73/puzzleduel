ef8f43fe1376d90c2ca1c07763bf9f728610e9ad
Andrey Bogdanov <bay73@mail.ru>
2020-10-22 09:46:44 +0200

    Add clues to Nurikabe puzzle

--- js/paint_square_puzzle.js
+++ js/paint_square_puzzle.js
@@ -41,7 +41,7 @@ innerCluePuzzle.prototype.initImages = function() {
     this.togglers = ["white", "star", "cross"];
   }
   if(this.typeCode == "nurikabe") {
-    this.clues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"];
+    this.clues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22"];
     this.togglers = ["white", "black", "cross"];
   }
   if(this.typeCode == "every_second_turn") {
