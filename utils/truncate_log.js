97ae569c277ef6c8590b6bdbb8875cecf347176f
Andrey Bogdanov <bay73@mail.ru>
2021-03-23 14:36:27 +0100

    Add utility to cleanup old logs

---
+++ utils/truncate_log.js
@@ -0,0 +1,32 @@
+const UserActionLog = require('../models/UserActionLog');
+
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// DB Config
+const db = require('../config/keys').mongoURI;
+
+truncate = async function () {
+  var d = new Date();
+  d.setDate(d.getDate()-31)
+  result = await UserActionLog.deleteMany({date: {$lt: d}});
+  console.log(result)
+}
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true, useUnifiedTopology: true }
+  )
+  .then(() => {
+    console.log('MongoDB Connected')
+    truncate().then(() => {
+      mongoose.disconnect();
+    });
+  })
+  .catch(err => console.log(err));
+
+
+
