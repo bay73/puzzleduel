91f3055f679e67a266fee5412d820cede5ae20e8
Andrey Bogdanov <bay73@mail.ru>
2021-07-12 15:51:40 +0200

    Add letters for tetrominoes

--- js/puzzle_figures.js
+++ js/puzzle_figures.js
@@ -5,14 +5,14 @@ gapTop: 2,
 snapId: 0,
 theme: 'default',
 
-tetro_I: [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:3,y:0}],
-tetro_I_vert: [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:0,y:3}],
-tetro_T: [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:1,y:1}],
-tetro_L: [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:1,y:2}],
-tetro_J: [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:-1,y:2}],
-tetro_S: [{x:0,y:0},{x:0,y:1},{x:1,y:1},{x:1,y:2}],
-tetro_Z: [{x:0,y:0},{x:0,y:1},{x:-1,y:1},{x:-1,y:2}],
-tetro_O: [{x:0,y:0},{x:1,y:0},{x:0,y:1},{x:1,y:1}],
+tetro_I: {letter: "I", letterPos: {x:1,y:0}, cells: [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:3,y:0}]},
+tetro_I_vert: {letter: "I", letterPos: {x:0,y:1}, cells: [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:0,y:3}]},
+tetro_T: {letter: "T", letterPos: {x:1,y:0}, cells: [{x:0,y:0},{x:1,y:0},{x:2,y:0},{x:1,y:1}]},
+tetro_L: {letter: "L", letterPos: {x:0,y:1}, cells: [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:1,y:2}]},
+tetro_J: {letter: "J", letterPos: {x:0,y:1}, cells: [{x:0,y:0},{x:0,y:1},{x:0,y:2},{x:-1,y:2}]},
+tetro_S: {letter: "S", letterPos: {x:0,y:1}, cells: [{x:0,y:0},{x:0,y:1},{x:1,y:1},{x:1,y:2}]},
+tetro_Z: {letter: "Z", letterPos: {x:0,y:1}, cells: [{x:0,y:0},{x:0,y:1},{x:-1,y:1},{x:-1,y:2}]},
+tetro_O: {letter: "O", letterPos: {x:0,y:0}, cells: [{x:0,y:0},{x:1,y:0},{x:0,y:1},{x:1,y:1}]},
 
 drawSquare: function(snap, x, y, size){
   var border = size/14;
@@ -26,9 +26,31 @@ drawSquare: function(snap, x, y, size){
   return path;
 },
 
-drawFigure: function(snap,coord,cells,size) {
-  var group = snap.g();
-  cells.forEach(function(cell) {
+drawLetter: function(snap, x, y, size, letter){
+  let topBorder = size*0.15;
+  let height = size*0.7;
+  let leftBorder = size*0.3;
+  let width = size*0.4;
+  if (letter=="I") {
+    leftBorder = size*0.4;
+    width = size*0.2;
+  }
+  let textElement = snap.text(puzzleFigures.gapLeft+x*size+leftBorder, puzzleFigures.gapTop+(y+0.6)*size+topBorder, letter);
+      
+  textElement.attr({
+    "font-family": "sans-serif",
+    "font-weight": "bold",
+     "lengthAdjust": "spacingAndGlyphs",
+     "fill": puzzleFigures.letterColor,
+     "font-size": height,
+     "textLength": width });
+  return textElement;
+},
+
+drawFigure: function(snap,coord,figure,size,withLetter) {
+  console.log(withLetter);
+  let group = snap.g();
+  figure.cells.forEach(function(cell) {
     let item = puzzleFigures.drawSquare(snap,coord.x+cell.x,coord.y+cell.y,size);
     group.add(item);
     item.click(function() {
@@ -39,10 +61,13 @@ drawFigure: function(snap,coord,cells,size) {
       });
     });
   });
+  if (withLetter) {
+    puzzleFigures.drawLetter(snap, coord.x+figure.letterPos.x, coord.y+figure.letterPos.y, size, figure.letter);
+  }
   group.attr({"isMarked": false})
 },
 
-createTetro7: function(snap) {
+createTetro7: function(snap, withLetters) {
   let width = $(snap.node).parent().width();
   const viewportWidth = Math.max(
     document.documentElement.clientWidth,
@@ -57,44 +82,47 @@ createTetro7: function(snap) {
     }
     snap.node.setAttribute("height", cellSize * 4 + puzzleFigures.gapTop*2);
     puzzleFigures.gapLeft = (width - cellSize*20)/2
-    puzzleFigures.drawFigure(snap,{x:0,y:0}, puzzleFigures.tetro_O, cellSize);
-    puzzleFigures.drawFigure(snap,{x:4,y:0}, puzzleFigures.tetro_Z, cellSize);
-    puzzleFigures.drawFigure(snap,{x:6,y:0}, puzzleFigures.tetro_S, cellSize);
-    puzzleFigures.drawFigure(snap,{x:9,y:0}, puzzleFigures.tetro_I_vert, cellSize);
-    puzzleFigures.drawFigure(snap,{x:11,y:0}, puzzleFigures.tetro_L, cellSize);
-    puzzleFigures.drawFigure(snap,{x:15,y:0}, puzzleFigures.tetro_J, cellSize);
-    puzzleFigures.drawFigure(snap,{x:17,y:0}, puzzleFigures.tetro_T, cellSize);
+    puzzleFigures.drawFigure(snap,{x:0,y:0}, puzzleFigures.tetro_O, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:4,y:0}, puzzleFigures.tetro_Z, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:6,y:0}, puzzleFigures.tetro_S, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:9,y:0}, puzzleFigures.tetro_I_vert, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:11,y:0}, puzzleFigures.tetro_L, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:15,y:0}, puzzleFigures.tetro_J, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:17,y:0}, puzzleFigures.tetro_T, cellSize, withLetters);
   } else {
     var cellSize = width/8;
     snap.node.setAttribute("height", cellSize * 8 + puzzleFigures.gapTop*2);
     puzzleFigures.gapLeft = (width - cellSize*8)/2
-    puzzleFigures.drawFigure(snap,{x:3,y:0}, puzzleFigures.tetro_O, cellSize);
-    puzzleFigures.drawFigure(snap,{x:1,y:0}, puzzleFigures.tetro_Z, cellSize);
-    puzzleFigures.drawFigure(snap,{x:6,y:0}, puzzleFigures.tetro_S, cellSize);
-    puzzleFigures.drawFigure(snap,{x:2,y:3}, puzzleFigures.tetro_I, cellSize);
-    puzzleFigures.drawFigure(snap,{x:2.5,y:5}, puzzleFigures.tetro_T, cellSize);
-    puzzleFigures.drawFigure(snap,{x:0,y:5}, puzzleFigures.tetro_L, cellSize);
-    puzzleFigures.drawFigure(snap,{x:7,y:5}, puzzleFigures.tetro_J, cellSize);
+    puzzleFigures.drawFigure(snap,{x:3,y:0}, puzzleFigures.tetro_O, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:1,y:0}, puzzleFigures.tetro_Z, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:6,y:0}, puzzleFigures.tetro_S, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:2,y:3}, puzzleFigures.tetro_I, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:2.5,y:5}, puzzleFigures.tetro_T, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:0,y:5}, puzzleFigures.tetro_L, cellSize, withLetters);
+    puzzleFigures.drawFigure(snap,{x:7,y:5}, puzzleFigures.tetro_J, cellSize, withLetters);
   }
 },
 
 init: function(element) {
   puzzleFigures.theme = element.attr('theme')?element.attr('theme'):'default';
   puzzleFigures.color = "#000";
+  puzzleFigures.letterColor = "#fff";
   puzzleFigures.markedColor = "#384d52";
   if (puzzleFigures.theme == "contrast"){
     puzzleFigures.color = "#fff";
+    puzzleFigures.letterColor = "#000";
     puzzleFigures.markedColor = "#404040";
   }
   if (puzzleFigures.theme == "white"){
-  puzzleFigures.markedColor = "#ddd";
+    puzzleFigures.markedColor = "#ddd";
   }
   puzzleFigures.snapId++;
   element.append('<svg id="figures_svg_' + puzzleFigures.snapId+'"></svg><br>');
 
   let figures = element.attr('figures');
+  let withLetters = (element.attr('letters')=="true");
   if (figures=="tetro7") {
-    puzzleFigures.createTetro7(Snap('#figures_svg_' + puzzleFigures.snapId));
+    puzzleFigures.createTetro7(Snap('#figures_svg_' + puzzleFigures.snapId), withLetters);
   }
 },
 
