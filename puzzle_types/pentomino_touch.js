d8a31804918a09a00734bd0a1f675ccef4ddc84a
Andrey Bogdanov <bay73@mail.ru>
2021-08-18 00:04:06 +0200

    Extract common utilities to work with pentomino

--- puzzle_types/pentomino_touch.js
+++ puzzle_types/pentomino_touch.js
@@ -1,72 +1,7 @@
 const util = require('./util');
+const pentomino_util = require('./pentomino_util');
 
 const Checker = {
-pentominoes: {
- "0-0,1-0,0-1,-1-1,0-2,": "F",
- "0-0,0-1,1-1,-1-1,1-2,": "F",
- "0-0,0-1,1-1,0-2,-1-2,": "F",
- "0-0,0-1,1-1,2-1,1-2,": "F",
- "0-0,1-0,1-1,2-1,1-2,": "F",
- "0-0,0-1,-1-1,-2-1,-1-2,": "F",
- "0-0,0-1,-1-1,0-2,1-2,": "F",
- "0-0,0-1,1-1,-1-1,-1-2,": "F",
- "0-0,1-0,2-0,3-0,4-0,": "I",
- "0-0,0-1,0-2,0-3,0-4,": "I",
- "0-0,0-1,0-2,0-3,1-3,":"L",
- "0-0,1-0,0-1,2-0,3-0,":"L",
- "0-0,1-0,1-1,1-2,1-3,":"L",
- "0-0,0-1,-1-1,-2-1,-3-1,":"L",
- "0-0,0-1,0-2,0-3,-1-3,":"L",
- "0-0,0-1,1-1,2-1,3-1,":"L",
- "0-0,1-0,0-1,0-2,0-3,":"L",
- "0-0,1-0,2-0,3-0,3-1,":"L",
- "0-0,0-1,-1-1,-1-2,-1-3,": "N",
- "0-0,1-0,2-0,2-1,3-1,": "N",
- "0-0,0-1,0-2,-1-2,-1-3,": "N",
- "0-0,1-0,1-1,2-1,3-1,": "N",
- "0-0,0-1,1-1,1-2,1-3,": "N",
- "0-0,1-0,0-1,-1-1,-2-1,": "N",
- "0-0,0-1,0-2,1-2,1-3,": "N",
- "0-0,1-0,0-1,2-0,-1-1,": "N",
- "0-0,1-0,0-1,1-1,0-2,": "P",
- "0-0,1-0,2-0,1-1,2-1,": "P",
- "0-0,0-1,-1-1,0-2,-1-2,": "P",
- "0-0,1-0,0-1,1-1,2-1,": "P",
- "0-0,1-0,0-1,1-1,1-2,": "P",
- "0-0,1-0,0-1,1-1,-1-1,": "P",
- "0-0,0-1,1-1,0-2,1-2,": "P",
- "0-0,1-0,0-1,2-0,1-1,": "P",
- "0-0,1-0,2-0,1-1,1-2,": "T",
- "0-0,0-1,-1-1,0-2,-2-1,": "T",
- "0-0,0-1,0-2,1-2,-1-2,": "T",
- "0-0,0-1,1-1,0-2,2-1,": "T",
- "0-0,0-1,1-1,2-1,2-0,": "U",
- "0-0,1-0,0-1,0-2,1-2,": "U",
- "0-0,1-0,0-1,2-0,2-1,": "U",
- "0-0,1-0,1-1,1-2,0-2,": "U",
- "0-0,0-1,0-2,1-2,2-2,": "V",
- "0-0,1-0,0-1,2-0,0-2,": "V",
- "0-0,1-0,2-0,2-1,2-2,": "V",
- "0-0,0-1,0-2,-1-2,-2-2,": "V",
- "0-0,0-1,1-1,1-2,2-2,": "W",
- "0-0,1-0,0-1,-1-1,-1-2,": "W",
- "0-0,1-0,1-1,2-1,2-2,": "W",
- "0-0,0-1,-1-1,-1-2,-2-2,": "W",
- "0-0,0-1,1-1,-1-1,0-2,": "X",
- "0-0,0-1,1-1,0-2,0-3,": "Y",
- "0-0,1-0,2-0,3-0,2-1,": "Y",
- "0-0,0-1,0-2,-1-2,0-3,": "Y",
- "0-0,0-1,1-1,-1-1,2-1,": "Y",
- "0-0,0-1,-1-1,0-2,0-3,": "Y",
- "0-0,0-1,1-1,-1-1,-2-1,": "Y",
- "0-0,0-1,0-2,1-2,0-3,": "Y",
- "0-0,1-0,2-0,1-1,3-0,": "Y",
- "0-0,1-0,1-1,1-2,2-2,": "Z",
- "0-0,0-1,-1-1,-2-1,-2-2,": "Z",
- "0-0,1-0,0-1,0-2,-1-2,": "Z",
- "0-0,0-1,1-1,2-1,2-2,": "Z",
-},
-
 check:function(dimension, clues, data){
   // Create array
   var part = dimension.split("-");
@@ -111,7 +46,7 @@ check:function(dimension, clues, data){
       }
     }
   }
-  var res = Checker.checkPento(cells, requiredLetters);
+  var res = pentomino_util.checkPento(cells, requiredLetters);
   if (res.status != "OK") {
     return res;
   }
@@ -122,25 +57,6 @@ check:function(dimension, clues, data){
   return {status: "OK"};
 },
 
-checkPento: function(cells, requiredLetters) {
-  var start = {x:0, y:0};
-  var map = util.create2DArray(cells.rows, cells.cols, "")
-  while (true) {
-    let res = Checker.findNextPentomino(cells, start, map, requiredLetters);
-    if (!res) break;
-    if (res.status != "OK") {
-      return res;
-    }
-  }
-  var res = Checker.checkAllUsedOnce(map, requiredLetters);
-  if (res.status != "OK") {
-    return res;
-  }
-  return {status: "OK"};
-},
-ifCell: function(cells, x, y) {
-  return x>=0 && x<cells.cols && y>=0 && y<cells.rows && cells[y][x];
-},
 checkClues: function(cells, clue) {
   for (var x = 0; x < clue.cols-1; x++) {
     for (var y = 0; y < clue.rows-1; y++) {
@@ -157,87 +73,6 @@ checkClues: function(cells, clue) {
   }
   return {status: "OK"};
 },
-findNextPentomino: function(cells, start, map, requiredLetters) {
-  while (true) {
-    if (start.x>=cells.cols) {
-      start.x = 0;
-      start.y++;
-    } else if (start.y >= cells.rows) {
-      return false;
-    } else {
-      if (cells[start.y][start.x] && map[start.y][start.x]=="") {
-        let area = Checker.buildArea(cells, start, map);
-        if (area.cells.length != 5) {
-          return {status: "Each marked area should form a pentomino from the given set" , errors: area.coords};
-        }
-        var pento = Checker.pentominoes[area.code];
-        if (typeof pento == "undefined" || !requiredLetters.includes(pento)) {
-          return {status: "Each marked area should form a pentomino from the given set" , errors: area.coords};
-        }
-        for (let i=0;i<area.cells.length;i++) {
-          map[area.cells[i].y][area.cells[i].x]=pento;
-        }
-        return {status: "OK"};
-      }
-      start.x++;
-    }
-  }
-  return false;
-},
-checkAllUsedOnce: function(map, requiredLetters) {
-  let lettersRequired = {};
-  let letters = {};
-  for (let i=0;i<requiredLetters.length;i++) {
-    let letter = requiredLetters.charAt(i);
-    if (!lettersRequired[letter]) {
-      letters[letter] = [];
-      lettersRequired[letter] = 5;
-    } else {
-      lettersRequired[letter] += 5;
-    }
-  }
-  for (var x = 0; x < map.cols; x++) {
-    for (var y = 0; y < map.rows; y++) {
-      if (map[y][x]!="") {
-        letters[map[y][x]].push(util.coord(x,y));
-      }
-    }
-  }
-  for(let letter of Object.keys(letters)){
-    if(letters[letter].length > lettersRequired[letter]) {
-      return {status: "Each element should be used exactly same number of times as given" , errors: letters[letter]};
-    }
-  }
-  for(let letter of Object.keys(letters)){
-    if(letters[letter].length < lettersRequired[letter]) {
-      return {status: "Each element should be used exactly same number of times as given" , errors: letters[letter]};
-    }
-  }
-  return {status: "OK"};
-},
-buildArea: function(cells, start, map) {
-   var area = {
-     cells: [],
-     coords: [],
-     code: ""
-   };
-   var queue = [];
-   queue.push(start);
-   while (queue.length > 0) {
-     let next = queue.shift();
-     if (Checker.ifCell(cells, next.x, next.y) && map[next.y][next.x]=="") {
-       map[next.y][next.x]=".";
-       area.cells.push({x: next.x, y: next.y});
-       area.coords.push(util.coord(next.x, next.y));
-       area.code+=(next.x-start.x)+"-"+(next.y-start.y)+",";
-       queue.push({x: next.x+1, y: next.y});
-       queue.push({x: next.x-1, y: next.y});
-       queue.push({x: next.x, y: next.y+1});
-       queue.push({x: next.x, y: next.y-1});
-     }
-   }
-   return area;
-}
 };
 
 module.exports = Checker;
