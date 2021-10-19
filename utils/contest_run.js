a43dcd15e784782f60a27a79e44c2128135f01d0
Andrey Bogdanov <bay73@mail.ru>
2021-10-19 20:08:48 +0200

    Prepare to Doubleshow 4

--- utils/contest_run.js
+++ utils/contest_run.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const db = require('../config/keys').mongoURI;
 
 recount = async function () {
-  await recountContest('5177acdef9fty');
+  await recountContest('40644y1kupynynq');
 }
 
 // Connect to MongoDB
