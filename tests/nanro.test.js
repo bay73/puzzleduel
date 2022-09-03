26531f2855f1dbcab2630752cb9abd67f656c68b
Andrey Bogdanov <bay73@mail.ru>
2022-09-03 18:40:18 +0200

    Fix Nanro tests

--- tests/nanro.test.js
+++ tests/nanro.test.js
@@ -41,7 +41,7 @@ test('Solver controllers',(suite) => {
   {"a1": "2", "a3":"3", "d2":"1", "areas": [["a1","a2","b1","b2"],["d1","d2"],["a4","b4"],["d4"],["c1","c2","a3","b3","c3","d3","c4"]]});
   puzzle.start();
 
-  assert("Empty cell chooser").that(puzzle.cells[2][2].chooserValues).containsExactly([{},{text: '1', returnValue: '1'},{text: '2', returnValue: '2'},{text: '3', returnValue: '3'},{text: '4', returnValue: '4'},{text: '5', returnValue: '5'},{text: '6', returnValue: '6'},{image: "cross"}]);
+  assert("Empty cell chooser").that(puzzle.cells[2][2].chooserValues).containsAtLeast([{},{text: '1', returnValue: '1'},{text: '2', returnValue: '2'},{text: '3', returnValue: '3'},{text: '4', returnValue: '4'},{text: '5', returnValue: '5'},{text: '9', returnValue: '9'},{image: "cross"}]);
   assert("Empty cell click").that(puzzle.cells[2][2].clickSwitch).isNull();
   assert("Clue cell chooser").that(puzzle.cells[0][0].chooserValues).isNull();
   assert("Clue cell click").that(puzzle.cells[0][0].clickSwitch).isNull();
@@ -53,7 +53,7 @@ test('Author controllers',(suite) => {
   let puzzle = suite.showPuzzle("nanro", "4x4");
   puzzle.edit();
 
-  assert("Cell chooser").that(puzzle.cells[0][0].chooserValues).containsExactly([{},{text: '1', returnValue: '1'},{text: '2', returnValue: '2'},{text: '3', returnValue: '3'},{text: '4', returnValue: '4'},{text: '5', returnValue: '5'},{text: '6', returnValue: '6'}]);
+  assert("Cell chooser").that(puzzle.cells[0][0].chooserValues).containsAtLeast([{},{text: '1', returnValue: '1'},{text: '2', returnValue: '2'},{text: '3', returnValue: '3'},{text: '4', returnValue: '4'},{text: '5', returnValue: '5'},{text: '9', returnValue: '9'}]);
   assert("Cell click").that(puzzle.cells[0][0].clickSwitch).isNull();
   assert("Edge chooser").that(puzzle.edges[0][0][1].chooserValues).isNull();
   assert("Edge click").that(puzzle.edges[0][0][1].clickSwitch).containsExactly([{}, {color: puzzle.colorSchema.gridColor, returnValue: 'bold'}]);
