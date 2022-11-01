e602dd6557fe5bfa5eaf75d12f72e324928081bb
Andrey Bogdanov <bay73@mail.ru>
2022-11-01 09:31:33 +0100

    Recount Nikoli contest

--- utils/contest_run.js
+++ utils/contest_run.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const db = require('../config/keys').mongoURI;
 
 recount = async function () {
-  await recountContest('yu6541kuy82cvs');
+  await recountContest('40645j1l9dyfi6u');
 }
 
 // Connect to MongoDB
