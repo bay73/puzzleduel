44590e3257b194d1a0e438f0067cbb497b1a78f8
Andrey Bogdanov <bay73@mail.ru>
2024-04-17 16:39:33 +0200

    Add IP blocker

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const expressip = require('express-ip');
 const cookieParser = require('cookie-parser');
 const profiler = require('./utils/profiler');
 const cache = require('./utils/cache');
+const ipfilter = require('express-ipfilter').IpFilter
 
 const app = express();
 
@@ -20,6 +21,12 @@ mongoose
 
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
+
+// Deny the following IPs
+const deniedIps = ['157.90.209.77']
+
+// Create the server
+app.use(ipfilter(deniedIps))
 
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({limit: '1mb', parameterLimit: 10000, extended: true}));
