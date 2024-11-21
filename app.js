6c343c2cab267f8981c4a643c5da5acb9e8c979e
Andrey Bogdanov <bay73@mail.ru>
2024-11-21 22:40:53 +0100

    Preserve connections globally

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ const expressip = require('express-ip');
 const cookieParser = require('cookie-parser');
 const profiler = require('./utils/profiler');
 const cache = require('./utils/cache');
+const { connectDBs } = require('./config/db')
 
 const app = express();
+
+connectDBs()
 
 require('dotenv').config();
 require('./config/passport')(passport);
