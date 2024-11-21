4ea7eeaa13fedc2987e50f8dcc0088c8ce174859
Andrey Bogdanov <bay73@mail.ru>
2024-11-21 22:51:31 +0100

    Reapply "Preserve connections globally"
    This reverts commit 4053a4a1ac25888f33c2fb062e2a96f7b677b2b8.

--- config/db.js
+++ config/db.js
@@ -4,11 +4,23 @@ require('dotenv').config();
 
 const config = require('../config/keys');
 
+var db
+var logDb
 
 const connectDBs = () => {
     try {
-        const db = mongoose.createConnection(config.mongoURI)
-        const logDb = mongoose.createConnection(config.mongoLogURI)
+        if (db == undefined) {
+          db = mongoose.createConnection(config.mongoURI)
+          db.on('connected', () => console.log('Db connected'));
+          db.on('open', () => console.log('Db open'));
+          db.on('disconnected', () => console.log('Db disconnected'));
+        }
+        if (logDb == undefined) {
+          logDb = mongoose.createConnection(config.mongoLogURI)
+          logDb.on('connected', () => console.log('logDb connected'));
+          logDb.on('open', () => console.log('logDb open'));
+          logDb.on('disconnected', () => console.log('logDb disconnected'));
+        }
         return { db, logDb }
     } catch (error) {
         console.error(`Error:${error.message}`)
