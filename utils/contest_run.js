fed7d1bd024061f14885235d40a1375dfc471c14
AndreyBogdanov <andrey.bogdanov@cobrainer.com>
2025-06-19 09:40:48 +0200

    Initialize Puzzle XL contests

--- utils/contest_run.js
+++ utils/contest_run.js
@@ -3,7 +3,7 @@ const recountContest = require('../utils/contest').recountContest;
 const mongoose = require('mongoose');
 
 recount = async function () {
-  await recountContest('stovtriaiujamsa');
+  await recountContest('lrtz8fw4j8fcaqmy1');
 }
 
 recount().then(() => {
