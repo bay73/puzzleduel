2eee013355f192ac3b872184accd1c632e5463aa
AndreyBogdanov <andrey.bogdanov@cobrainer.com>
2025-09-01 13:29:15 +0200

    Start ring-ring contest

--- utils/contest_run.js
+++ utils/contest_run.js
@@ -3,7 +3,7 @@ const recountContest = require('../utils/contest').recountContest;
 const mongoose = require('mongoose');
 
 recount = async function () {
-  await recountContest('lrtz8fw4j8fcaqmy1');
+  await recountContest('sringriaiujamsa');
 }
 
 recount().then(() => {
