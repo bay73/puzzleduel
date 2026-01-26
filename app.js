9e529481d5daa830e48e192398b6626c75ca100b
Andrey Bogdanov <andrey.bogdanov@eudi.sprind.org>
2026-01-26 23:39:45 +0100

    Add rediect from www to non-www

--- app.js
+++ app.js
@@ -18,6 +18,19 @@ require('./config/passport')(passport);
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
 
+app.enable('trust proxy');
+
+app.use((req, res, next) => {
+  const host = req.headers.host;
+
+  if (host === 'www.puzzleduel.club') {
+    return res.redirect(301, `https://puzzleduel.club${req.originalUrl}`);
+  }
+
+  next();
+});
+
+
 app.use(express.json({limit: '1mb'}));
 app.use(express.urlencoded({limit: '1mb', parameterLimit: 10000, extended: true}));
 app.use(expressip().getIpInfoMiddleware);
