1fd63fd274e80287cff8df5158d5a63a6798f924
Andrey Bogdanov <andrey.bogdanov@eudi.sprind.org>
2026-07-25 17:13:13 +0200

    Prepare the next contest

--- utils/contest_run.js
+++ utils/contest_run.js
@@ -3,7 +3,7 @@ const recountContest = require('../utils/contest').recountContest;
 const mongoose = require('mongoose');
 
 recount = async function () {
-  await recountContest('xlutdfbohbjhgdm');
+  await recountContest('dubleurvmbmlkjl');
 }
 
 recount().then(() => {
