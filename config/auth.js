cb3eb4b6ba88cd8b229e976735364d4f33adf2e5
Andrey Bogdanov <andrey.bogdanov@eudi.sprind.org>
2026-09-24 10:17:46 +0200

    Migrate to the new version of connect-mongo

--- config/auth.js
+++ config/auth.js
@@ -1,5 +1,5 @@
 const session = require('express-session');
-const MongoStore = require('connect-mongo');
+const { MongoStore } = require('connect-mongo');
 
 module.exports = {
   ensureAuthenticated: function(req, res, next) {
